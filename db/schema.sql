48d477f1f006c3bdae2b80f601b06d63324a81b0
Dschnepf7 <domschnepf1@gmail.com>
2023-03-01 17:56:57 -0600

    continued working on datasets and seeds in MySQL

--- db/schema.sql
+++ db/schema.sql
@@ -5,14 +5,14 @@ USE database_db;
 
 CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHCHAR(30)
+    department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHCHAR(30),
+    title VARCHAR(30),
     salary DECIMAL,
-    department_id INT,
+    department_id INT
 );
 
 CREATE TABLE employee(
@@ -20,5 +20,5 @@ CREATE TABLE employee(
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT,
-)
+    manager_id INT
+);
---
+++ db/seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO department (id, department_name)
+VALUES (1,'sales'),
+        (2,'human resources'),
+        (3,'management'),
+        (4,'software');
+
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1,'software director', 100000.00, 4),
+    (2,'manager', 75000.00, 3),
+    (3,'human resources consultant', 75000.00, 2),
+    (4,'Sales Representative', 50000.00, 1);
+
+INSERT INTO employee (id, first_name, last_name, role_id, department.id)
+VALUES (1,'Dominic', 'Schnepf', 1, 4),
+        (2, 'John', 'Doe', 2, 3),
+        (3, 'Jane', 'Doe',3, 2),
+        (4, 'Michael', 'Scott',4, 1);
+
