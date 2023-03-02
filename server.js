8de1588ccf5ef3298b157e995354dde6f6eaad81
Dschnepf7 <domschnepf1@gmail.com>
2023-03-01 18:33:14 -0600

    added server.js script to establish connection to database

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const inquirer = require('inquirer');
+
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: '050497Ds!',
+    database: 'database_db'
+  },
+  console.log(`Connected to database.`)
+);
