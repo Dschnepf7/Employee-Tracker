c6823a4437ccf910668664ac3783bd821a64a97d
Dschnepf7 <domschnepf1@gmail.com>
2023-03-01 19:23:56 -0600

    edited schema, added console.table function, added inquirer functionality

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,18 +2,18 @@ INSERT INTO department (id, department_name)
 VALUES (1,'sales'),
         (2,'human resources'),
         (3,'management'),
-        (4,'software');
+        (4,'accounting');
 
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (1,'software director', 100000.00, 4),
-    (2,'manager', 75000.00, 3),
+VALUES (1,'accountant', 100000.00, 4),
+    (2,'manager', 150000.00, 3),
     (3,'human resources consultant', 75000.00, 2),
     (4,'Sales Representative', 50000.00, 1);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1,'Dominic', 'Schnepf', 1, 4),
-        (2, 'John', 'Doe', 2, 3),
-        (3, 'Jane', 'Doe',3, 2),
-        (4, 'Michael', 'Scott',4, 1);
+VALUES (1,'Oscar', 'Martinez', 1, 2),
+        (2, 'Dwight', 'Schrute', 4, 2),
+        (3, 'Toby', 'Fletcher',3, 2),
+        (4, 'Michael', 'Scott',2, NULL);
 
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const inquirer = require('inquirer');
+//import and require console.table
+const cTable = require('console.table');
+
 
 // Import and require mysql2
 const mysql = require('mysql2');
@@ -24,6 +27,127 @@ const db = mysql.createConnection(
   console.log(`Connected to database.`)
 );
 
-db.query('SELECT * FROM employee', function (err, results) {
-    console.log(results);
+// db.query('SELECT * FROM employee', function (err, results) {
+//     console.log(results);
+//   });
+
+
+  const mainMenuOptions = [
+    'View all departments',
+    'View all roles',
+    'View all employees',
+    'Add a department',
+    'Add a role',
+    'Add an employee',
+    'Update an employee role',
+  ];
+  
+  // Define the inquirer questions for adding a department
+  const addDepartmentQuestions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Enter the name of the department:',
+    },
+  ];
+  
+  // Define the inquirer questions for adding a role
+  const addRoleQuestions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Enter the title of the role:',
+    },
+    {
+      type: 'number',
+      name: 'salary',
+      message: 'Enter the salary for the role:',
+    },
+    {
+      type: 'number',
+      name: 'department_id',
+      message: 'Enter the department ID for the role:',
+    },
+  ];
+  
+  // Define the inquirer questions for adding an employee
+  const addEmployeeQuestions = [
+    {
+      type: 'input',
+      name: 'first_name',
+      message: 'Enter the employee\'s first name:',
+    },
+    {
+      type: 'input',
+      name: 'last_name',
+      message: 'Enter the employee\'s last name:',
+    },
+    {
+      type: 'number',
+      name: 'role_id',
+      message: 'Enter the role ID for the employee:',
+    },
+    {
+      type: 'number',
+      name: 'manager_id',
+      message: 'Enter the manager ID for the employee:',
+    },
+  ];
+  
+  // Define the inquirer questions for updating an employee's role
+  const updateEmployeeRoleQuestions = [
+    {
+      type: 'number',
+      name: 'employee_id',
+      message: 'Enter the ID of the employee to update:',
+    },
+    {
+      type: 'number',
+      name: 'new_role_id',
+      message: 'Enter the new role ID for the employee:',
+    },
+  ];
+  
+
+  inquirer.prompt({
+    type: 'list',
+    name: 'mainMenuChoice',
+    message: 'What would you like to do?',
+    choices: mainMenuOptions,
+  })
+  .then((answer) => {
+    // Handle the selected main menu option
+    switch (answer.mainMenuChoice) {
+      case 'View all departments':
+        // Code to view all departments
+    db.query('SELECT * FROM department', function (err, results) {
+        console.table(results);
   });
+
+        break;
+      case 'View all roles':
+        // Code to view all roles
+        db.query('SELECT * FROM role', function (err, results) {
+            console.table(results);
+      });
+        break;
+      case 'View all employees':
+        // Code to view all employees
+        db.query('SELECT * FROM employee', function (err, results) {
+            console.table(results);
+      });
+        break;
+      case 'Add a department':
+        // Prompt the user for the department name and add it to the database
+        inquirer.prompt(addDepartmentQuestions)
+        .then((answer) => {
+          // Code to add a department
+        });
+        break;
+      case 'Add a role':
+        // Prompt the user for the role information and add it to the database
+        inquirer.prompt(addRoleQuestions)
+        .then((answer) => {
+          // Code to add a role
+        });
+        break;}})
